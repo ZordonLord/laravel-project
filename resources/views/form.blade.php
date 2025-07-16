c363f5cf0c17eb0008d3097456833005908431a4
ZordonLord <zordonlord@mail.ru>
2025-07-17 00:23:43 +1000

    add метод index

---
+++ resources/views/form.blade.php
@@ -0,0 +1,13 @@
+<form method="POST" action="">
+    @csrf
+    <label for="first_name">Имя:</label>
+    <input type="text" id="first_name" name="first_name" required><br>
+
+    <label for="last_name">Фамилия:</label>
+    <input type="text" id="last_name" name="last_name" required><br>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required><br>
+
+    <button type="submit">Отправить</button>
+</form> 

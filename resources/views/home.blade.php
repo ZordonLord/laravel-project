ee1c4ada10fc23a8a99d10a17b9b4c111f6c7452
ZordonLord <zordonlord@mail.ru>
2025-07-17 00:48:21 +1000

    Добавлена базовая HTML-разметка

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,14 @@
-<!-- Шаблон домашней страницы --> 
+@extends('layouts.default')
+
+@section('content')
+    <h2>Добро пожаловать, {{ $name }}!</h2>
+    <p>Возраст: 
+        @if($age > 18)
+            {{ $age }}
+        @else
+            <span style="color:red;">Указанный человек слишком молод.</span>
+        @endif
+    </p>
+    <p>Должность: {{ $position }}</p>
+    <p>Адрес: {{ $address }}</p>
+@stop 

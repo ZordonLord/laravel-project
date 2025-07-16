683a668a2a5d84606df1be1c5771265e4c375566
ZordonLord <zordonlord@mail.ru>
2025-07-17 00:44:27 +1000

    Созданы 2 шаблона и роуты

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,21 @@ use App\Http\Controllers\FormProcessor;
 use App\Models\Employee;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        'name' => 'Иван',
+        'age' => 30,
+        'position' => 'Разработчик',
+        'address' => 'Москва, ул. Примерная, 1',
+    ]);
+});
+
+Route::get('/contacts', function () {
+    return view('contacts', [
+        'address' => 'Санкт-Петербург, ул. Контактная, 2',
+        'post_code' => '190000',
+        'email' => 'contact@example.com',
+        'phone' => '+7 999 123-45-67',
+    ]);
 });
 
 Route::get('/userform', [FormProcessor::class, 'index']);
